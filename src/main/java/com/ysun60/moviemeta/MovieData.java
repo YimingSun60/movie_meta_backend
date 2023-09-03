80a0b8728f31746400a74f649ee6f616fec11ca5
YimingSun60 <ysun60@toki.waseda.jp>
2023-09-03 14:43:37 +0900

    Correct the type of some variables

---
+++ src/main/java/com/ysun60/moviemeta/MovieData.java
@@ -0,0 +1,28 @@
+package com.ysun60.moviemeta;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+//collection name should match the actual mongodata collection name
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieData {
+    @Id
+    private ObjectId id;
+    private String title;
+    private Integer year;
+    private List<String> cast;
+    private List<String> genres;
+    private String href;
+    private String extract;
+    private String thumbnail;
+    private Integer thumbnail_width;
+    private Integer thumbnail_height;
+}
