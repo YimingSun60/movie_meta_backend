c0f44287669aec0f0d22a6482ca3ce4b679fd328
YimingSun60 <ysun60@toki.waseda.jp>
2023-09-03 14:44:12 +0900

    Create MoiveMetaApplicationTests.java

---
+++ src/test/java/com/ysun60/moviemeta/MoiveMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysun60.moviemeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiveMetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
