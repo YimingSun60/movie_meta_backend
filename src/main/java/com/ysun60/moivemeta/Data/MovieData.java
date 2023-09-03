4f6be6200d8756f3734dcd90422a35bcb99cfd94
YimingSun60 <ysun60@toki.waseda.jp>
2023-09-03 14:41:45 +0900

    Change record class to traditional class

--- src/main/java/com/ysun60/moivemeta/Data/MovieData.java
+++
@@ -1,8 +0,0 @@
-package com.ysun60.moivemeta.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import java.util.Vector;
-
-@Document("moviedata")
-public record MovieData(@Id String ID, String title, int year, Vector cast, Vector genres, String href,
-                        String extract, String thumbnail, int thumbnail_width, int thumbnail_height) { }
