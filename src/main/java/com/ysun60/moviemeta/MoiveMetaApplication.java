0615ac4164c1032f1abca1855c5a6c1fa27706bf
YimingSun60 <ysun60@toki.waseda.jp>
2023-09-03 14:42:47 +0900

    Create MoiveMetaApplication.java

---
+++ src/main/java/com/ysun60/moviemeta/MoiveMetaApplication.java
@@ -0,0 +1,19 @@
+package com.ysun60.moviemeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiveMetaApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MoiveMetaApplication.class, args);
+
+    }
+
+
+
+
+
+
+}
