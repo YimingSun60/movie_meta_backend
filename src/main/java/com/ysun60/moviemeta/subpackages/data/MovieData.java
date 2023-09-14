d48efab2adfb3768a3ec06a85df9977737a75d38
YimingSun60 <ysun60@toki.waseda.jp>
2023-09-15 02:50:12 +0900

    Changed mongodb cluster

--- src/main/java/com/ysun60/moviemeta/subpackages/data/MovieData.java
+++ src/main/java/com/ysun60/moviemeta/subpackages/data/MovieData.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 //collection name should match the actual mongodata collection name
-@Document(collection = "movies")
+@Document(collection = "movie")
 @Data
 //@AllArgsConstructor
 @NoArgsConstructor
