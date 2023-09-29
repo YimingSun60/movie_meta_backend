5eac893311e68ac629dbe420a31406d5dfa62cfa
YimingSun60 <ysun60@toki.waseda.jp>
2023-09-29 17:24:19 +0900

    Add login and signup mapping

--- src/main/java/com/ysun60/moviemeta/subpackages/controller/MovieController.java
+++ src/main/java/com/ysun60/moviemeta/subpackages/controller/MovieController.java
@@ -1,7 +1,10 @@
 package com.ysun60.moviemeta.subpackages.controller;
 
 import com.ysun60.moviemeta.subpackages.data.MovieData;
+import com.ysun60.moviemeta.subpackages.data.User;
 import com.ysun60.moviemeta.subpackages.repository.MovieRepo;
+import com.ysun60.moviemeta.subpackages.service.MovieService;
+import com.ysun60.moviemeta.subpackages.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,31 +24,48 @@ public class MovieController {
     public ResponseEntity<String> Test() {
         return new ResponseEntity<>("HelloWorld", HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     @PostConstruct
     public void init() {
         System.out.println("MovieController initialized!");
     }
 
     @Autowired
-    private MovieRepo movieRepo;
+    private MovieService movieService;
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/search")
-    public ResponseEntity<List<MovieData>> findMovieByTitle(@RequestParam String title){
-        List<MovieData> movies = movieRepo.findMovieDataBytitleContainingIgnoreCase(title);
-        System.out.println("Movie: " + movies);
-        if (movies.isEmpty()) {
+    public ResponseEntity<List<MovieData>> findMovieByTitle(@RequestParam String title) {
+        List<MovieData> movies = movieService.findMovieByTitle(title);
+//        System.out.println("Movie: " + movies);
+        if (movies == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-            return new ResponseEntity<>(movies, HttpStatus.OK);
+        return new ResponseEntity<>(movies, HttpStatus.OK);
     }
+
 
     @GetMapping("/root")
-    public ResponseEntity<List<MovieData>> FindAllMovies(){
-        return new ResponseEntity<>(movieRepo.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<MovieData>> FindAllMovies() {
+        return new ResponseEntity<>(movieService.findAllMovies(), HttpStatus.OK);
     }
 
-    @GetMapping("/home")
-    public ResponseEntity<List<MovieData>> FindAmountofMovies(){
-        return new ResponseEntity<>(movieRepo.findAll(PageRequest.of(0,5)).getContent(), HttpStatus.OK);
+    @GetMapping("/user/login")
+    public ResponseEntity<User> findUserByUserName(@RequestParam String username) {
+        User user = userService.findUserByUserName(username);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
-
+    @PostMapping("/user/signup")
+    public ResponseEntity<User> createUser(@RequestParam String username, @RequestParam String password) {
+        User user = userService.createUser(username, password);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
